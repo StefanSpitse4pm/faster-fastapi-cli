f7e7f585fe011e9bcb4c3dda9d3929160495d2aa
stefanspitse4pm <stefan@4propert.ie>
2025-04-20 22:18:28 +0200

    Made changes to the location of tempate generation

--- setup.py
+++ setup.py
@@ -4,6 +4,10 @@ setup(
     name="fasterfastapi",
     version="0.0.1",
     packages=find_packages(),  # <== this will include the 'fasterfastapi' package
+    include_package_data=True,
+    package_data={
+        'your_package_name': ['templates/*.jinja'],  # adjust path as needed
+    },
     install_requires=[
         "Click",
         "Wheel",
